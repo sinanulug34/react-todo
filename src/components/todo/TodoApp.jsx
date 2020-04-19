a0826c6bc115ff9ca9ca3aed9c54ca67a5955b4f
Sinan Uluğ <sinanulug@aaas-MacBook-Pro.local>
2020-04-19 18:15:24 +0300

    added footer and logout component and .css file using bootstrap

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 class TodoApp extends Component {
     render() {
@@ -7,13 +7,16 @@ class TodoApp extends Component {
             <div className="TodoApp">
                 <Router>
                     <>
+                        <HeaderComponent />
                         <Switch>
                             <Route path="/" exact component={LoginComponent} />
                             <Route path="/login" component={LoginComponent} />
                             <Route path="/welcome/:name" component={WelcomeComponent} />
                             <Route path="/todos/" component={ListTodosComponent} />
+                            <Route path="/logout/" component={LogoutComponent} />
                             <Route component={ErrorComponent} />
                         </Switch>
+                        <FooterComponent />
                     </>
 
                 </Router>
@@ -24,7 +27,52 @@ class TodoApp extends Component {
 }
 class WelcomeComponent extends Component {
     render() {
-        return <div>Welcome {this.props.match.params.name}</div>
+        return <div>Welcome {this.props.match.params.name} You can manage your todos <Link to="/todos"> here</Link></div>
+    }
+}
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="https://tr.reactjs.org/tutorial/tutorial.html" className="navbar-brand">React Page</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li> <Link className="nav-link" to="/login">Login</Link></li>
+                        <li> <Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+
+                </nav>
+            </header>
+        )
+    }
+}
+class FooterComponent extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                    <span className="text-muted">
+                        All Rights Reserved 2020
+                    </span>
+            </footer>
+        )
+    }
+}
+class LogoutComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>You are logged out</h1>
+                <div className="container">
+                    Thank you for using our application
+                </div>
+               </div>
+        )
     }
 }
 
@@ -34,9 +82,9 @@ class ListTodosComponent extends Component {
         this.state = {
 
             todos: [
-                { id: 1, description: 'Learning React' },
-                { id: 2, description: 'Learning Spring Boot' },
-                { id: 3, description: 'Learning Java' }
+                { id: 1, description: 'Learning React', done: false, targetDate: new Date() },
+                { id: 2, description: 'Learning Spring Boot', done: false, targetDate: new Date() },
+                { id: 3, description: 'Learning Java', done: false, targetDate: new Date() }
             ]
         }
     }
@@ -47,10 +95,12 @@ class ListTodosComponent extends Component {
             <h1>List Todos</h1>
             <table>
                 <thead>
-                        <tr>
-                            <th>id </th>
-                            <th>description</th>
-                        </tr>
+                    <tr>
+                        <th>id </th>
+                        <th>description</th>
+                        <th>Target Date</th>
+                        <th>Is Completed?</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {
@@ -59,9 +109,12 @@ class ListTodosComponent extends Component {
                                 <tr>
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                         )
                     }
+
                 </tbody>
             </table>
 
